64aee00a91143f405a1dd95f509d3a84fadaa78a
keji <tomitoyo.93@gmail.com>
2020-10-22 13:05:36 +0100

    first commit

---
+++ birthday/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Reminder from './components/reminder'
+
+function App() {
+  return (
+    <div>
+    <Reminder />
+    </div>
+  );
+}
+
+export default App;
